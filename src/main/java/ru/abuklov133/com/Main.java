0d4b704e17f00fc6e0edc74828256643a348e7bd
abuklov133 <abuklov133@gmail.com>
2024-02-20 15:45:12 +0300

    Start Concurrency

---
+++ src/main/java/ru/abuklov133/com/Main.java
@@ -0,0 +1,21 @@
+package ru.abuklov133.com;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start");
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    System.out.print(i);
+                }
+            }
+        });
+        thread.start();
+        for (int i = 0; i < 1000; i++) {
+            System.out.print("M");
+        }
+        System.out.println("\nFinish");
+    }
+}
