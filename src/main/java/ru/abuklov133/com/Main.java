aeb93e7632e5232419b7b859cad8cdcb900ff68f
abuklov133 <abuklov133@gmail.com>
2024-02-23 11:09:08 +0300

    Потоки демоны

--- src/main/java/ru/abuklov133/com/Main.java
+++ src/main/java/ru/abuklov133/com/Main.java
@@ -1,21 +1,105 @@
 package ru.abuklov133.com;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Start");
+    private static final int SIZE = 10_000_000;
+    private static final int HALF = SIZE / 2;
 
-        Thread thread = new Thread(new Runnable() {
+    public static void main(String[] args) {
+        startTimer();
+        Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 1000; i++) {
-                    System.out.print(i);
+                withoutConcurrency();
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                whitConcurrency();
+            }
+        });
+        thread1.start();
+        thread2.start();
+    }
+
+    private static void startTimer() {
+        Thread timer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                try {
+                    while (true) {
+                        System.out.println(i++);
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
         });
-        thread.start();
-        for (int i = 0; i < 1000; i++) {
-            System.out.print("M");
+        timer.setDaemon(true);
+        timer.start();
+    }
+
+    public static void withoutConcurrency() {
+        float[] array = new float[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            array[i] = 1f;
         }
-        System.out.println("\nFinish");
+        long before = System.currentTimeMillis();
+        for (int i = 0; i < SIZE; i++) {
+            float f = (float) i;
+            array[i] = (float) (array[i] * Math.sin(0.2f + f / 5)
+                    * Math.cos(0.2f + f / 5) * Math.cos(0.4f + f / 2));
+        }
+        long after = System.currentTimeMillis();
+        System.out.println("withoutConcurrency :" + (after - before));
+    }
+
+    public static void whitConcurrency() {
+        float[] array = new float[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            array[i] = 1f;
+        }
+        long before = System.currentTimeMillis();
+        float[] firstHalf = new float[HALF];
+        float[] secondHalf = new float[HALF];
+        System.arraycopy(array, 0, firstHalf, 0, HALF);
+        System.arraycopy(array, HALF, secondHalf, 0, HALF);
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < HALF; i++) {
+                    float f = (float) (i);
+                    firstHalf[i] = (float) (firstHalf[i] * Math.sin(0.2f + f / 5)
+                            * Math.cos(0.2f + f / 5) * Math.cos(0.4f + f / 2));
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < HALF; i++) {
+                    float f = (float) (i + +HALF);
+                    secondHalf[i] = (float) (secondHalf[i] * Math.sin(0.2f + i / 5)
+                            * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.arraycopy(firstHalf, 0, array, 0, HALF);
+        System.arraycopy(secondHalf, 0, array, HALF, HALF);
+        long after = System.currentTimeMillis();
+        System.out.println("withconcurrency : " + (after - before));
     }
 }
+
+
+
